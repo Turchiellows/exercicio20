1bc15779fb7dd5ceda09bdcaf11d458c0cfde78f
root <root@DELL-7559>
2024-09-18 21:22:53 -0300

    Início do projeto

---
+++ src/main/java/Exercicio20.java
@@ -0,0 +1,23 @@
+// IMPORTS
+
+import java.util.Scanner;
+
+public class Exercicio20 {
+
+	public static void main(String[] args) {
+		// INSTANTIATE
+		Scanner sc = new Scanner(System.in);
+
+		// DECLARATE
+		int n;
+
+		// COLLECT DATA
+		System.out.print("NA : ");
+		n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			System.out.println(i);
+		}
+
+	}
+}
