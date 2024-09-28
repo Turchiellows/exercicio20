0f389ade059874138d53c2df338ff2a76497d462
Turchiellows <>
2024-09-28 14:26:10 -0300

    refactor: Mudança de indentação

--- src/main/java/Exercicio20.java
+++ src/main/java/Exercicio20.java
@@ -4,23 +4,21 @@ import java.util.Scanner;
 
 public class Exercicio20 {
 
-	public static void main(String[] args) {
-		// INSTANTIATE
-		Scanner sc = new Scanner(System.in);
+   public static void main(String[] args) {
+      // INSTANTIATE
+      Scanner sc = new Scanner(System.in);
 
-		// DECLARATE
-		int n;
+      // DECLARATE
+      int n;
 
-		// COLLECT DATA
-		System.out.print("NA : ");
-		n = sc.nextInt();
+      // COLLECT DATA
+      System.out.print("NA : ");
+      n = sc.nextInt();
 
-		
-		// OUTPUT
-		for (int i = 1; i <= n; i++) {
-			System.out.println(i);
-		}
-		
+      // OUTPUT
+      for (int i = 1; i <= n; i++) {
+         System.out.println(i);
+      }
 
-	}
+   }
 }
